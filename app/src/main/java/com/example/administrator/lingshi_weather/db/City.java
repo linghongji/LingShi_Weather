d18ae27927c908cbed0085ee40918988887eb434
linghongji <linghongji@haixue.com>
2018-02-22 11:43:16 +0800

    加入数据库与表的各项配置

---
+++ app/src/main/java/com/example/administrator/lingshi_weather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.administrator.lingshi_weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2018/2/22.
+ */
+
+public class City extends DataSupport{
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private String provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(String provinceId) {
+        this.provinceId = provinceId;
+    }
+}
